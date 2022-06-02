dd682895e200932f2441735819b0257a9e75bb3c
TonyJHC <jongvin01@naver.com>
2022-06-02 15:09:37 +0900

    final commit

--- app/src/main/java/com/example/androidappforiot/MapsActivity.java
+++ app/src/main/java/com/example/androidappforiot/MapsActivity.java
@@ -3,6 +3,7 @@ package com.example.androidappforiot;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -164,94 +165,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         String id = marker.getSnippet();
         int idd = Integer.parseInt(id);
 
-        new Thread() {
-            @Override
-            public void run() {
 
-                String urlAddress = "https://907epdyfgc.execute-api.ap-northeast-2.amazonaws.com/prod/trash/shadow";
-                StringBuffer sbParams = new StringBuffer();
-
-                try {
-                    URL url = new URL(urlAddress);
-                    HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
-
-                    // [2-1]. urlConn 설정.
-                    urlConn.setConnectTimeout(15000);
-                    urlConn.setReadTimeout(5000);
-                    urlConn.setDoInput(true);
-                    urlConn.setDoOutput(true);
-                    urlConn.setUseCaches(false);
-                    urlConn.setRequestMethod("POST"); // URL 요청에 대한 메소드 설정 : POST.
-                    urlConn.setRequestProperty("Accept","application/json"); // Accept-Charset 설정.
-                    urlConn.setRequestProperty("Content-Type","applicaiton/json;utf-8");
-//                    urlConn.setRequestProperty("apikey", ""); // ""안에 apikey를 입력
-                    urlConn.setDoOutput(true);
-
-                    // Json 전송
-                    JSONObject jsonObject = new JSONObject();
-                    jsonObject.put("id",idd);
-
-                    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(urlConn.getOutputStream()));
-                    bw.write(jsonObject.toString());
-                    bw.flush();
-                    bw.close();
-
-                    // 서버 응답
-                    BufferedReader in = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
-                    String returnMsg = in.readLine();
-                    System.out.println("응답메시지: " + returnMsg);
-
-                    // 스레드에서 Toast 메시지 띄울 때
-                    Handler handler = new Handler(Looper.getMainLooper());
-                    handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run()
-                        {
-                            Toast.makeText(getApplicationContext(), returnMsg,Toast.LENGTH_LONG);
-
-                        }
-                    }, 0);
-
-                    // [2-2]. parameter 전달 및 데이터 읽어오기.
-//                    String strParams = sbParams.toString(); //sbParams에 정리한 파라미터들을 스트링으로 저장. 예)id=id1&pw=123;
-//                    OutputStream os = urlConn.getOutputStream();
-//                    os.write(strParams.getBytes("UTF-8")); // 출력 스트림에 출력.
-//                    os.flush(); // 출력 스트림을 플러시(비운다)하고 버퍼링 된 모든 출력 바이트를 강제 실행.
-//                    os.close(); // 출력 스트림을 닫고 모든 시스템 자원을 해제.
-
-                    // [2-3]. 연결 요청 확인.
-                    // 실패 시 null을 리턴하고 메서드를 종료.
-//                    if (urlConn.getResponseCode() != HttpURLConnection.HTTP_OK)
-//                        System.out.println("연결 실패");
-//
-//                    // [2-4]. 읽어온 결과물 리턴.
-//                    // 요청한 URL의 출력물을 BufferedReader로 받는다.
-//                    BufferedReader reader = new BufferedReader(new InputStreamReader(urlConn.getInputStream(), "UTF-8"));
-//
-//                    // 출력물의 라인과 그 합에 대한 변수.
-//                    String line;
-//                    String page = "";
-//
-//                    // 라인을 받아와 합친다.
-//                    while ((line = reader.readLine()) != null){
-//                        page += line;
-//                    }
-//
-////                    return page;
-
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }.start();
         CameraUpdate center = CameraUpdateFactory.newLatLng(marker.getPosition());
 
         mMap.animateCamera(center);
         marker.showInfoWindow(); // 정보 창 열기
+
+        String url = "https://907epdyfgc.execute-api.ap-northeast-2.amazonaws.com/prod/trash/shadow";
+        // 새로운 액티비티 실행
+        Intent intent = new Intent(MapsActivity.this, ClickActivity.class);
+        intent.putExtra("id",idd );//getLogsURL.getText().toString());
+        startActivity(intent);
         //
 //        changeSelectedMarker(marker);
 //
